34cfb7889fee97c8edf03556dc20198f37b52a70
Haritha <harithaanasuri@gmail.com>
2015-06-22 11:06:11 +0530

    Sample project

---
+++ TestProject/TestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by Haritha on 22/06/15.
+//  Copyright (c) 2015 Haritha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestProject/TestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by Haritha on 22/06/15.
+//  Copyright (c) 2015 Haritha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
